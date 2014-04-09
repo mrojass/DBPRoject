682acca80d8c70a8d2783985db1896c414feda24
mranderson123 <mca12@my.fsu.edu>
2014-04-09 15:45:41 -0400

    Created failed deploy page with a return button.

---
+++ Failed.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Deploy Failed</title>
+	</head>
+
+<body>
+
+	<h2>Deploy Failed. Something went wrong, please input valid information.</h2>
+	
+	<a href="Deploy.php"><input type="submit" value="Return"></a>
+</body>
